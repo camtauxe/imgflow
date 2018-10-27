d2935b0c4c16ae4315b20d1465484f5a5d268d74
abos <austinmbos@gmail.com>
2018-10-27 14:50:18 +0000

    brought in trunk changes

---
+++ imgflow/src/main/java/edu/nmsu/imgflow/GraphNodeColorEffects.java
@@ -0,0 +1,73 @@
+package edu.nmsu.imgflow;
+
+import javafx.scene.paint.Color;
+import javafx.scene.image.WritableImage;
+import javafx.scene.image.Image;
+import javafx.scene.image.PixelWriter;
+import javafx.scene.image.PixelReader;
+
+/**
+ * A type of graph node that can modify the hue, 
+ * saturation, and brightness of an image
+ */
+public class GraphNodeColorEffects extends GraphNode {
+
+    private NodePropertySlider hueSlider, saturationSlider, brightnessSlider;
+
+    private NodeSocketInput  in;
+    private NodeSocketOutput out;
+
+    public GraphNodeColorEffects() {
+        hueSlider           = new NodePropertySlider(this, "Hue",               0, 255, 0);
+        saturationSlider    = new NodePropertySlider(this, "Saturation (%)",    0, 250, 100);
+        brightnessSlider    = new NodePropertySlider(this, "Brightness (%)",    0, 250, 100);
+
+        properties.add(hueSlider);
+        properties.add(saturationSlider);
+        properties.add(brightnessSlider);
+
+        in  = inputSockets.get(0);
+        out = outputSockets.get(0);
+    }
+
+    public String getBaseName() { return "Color Effects"; }
+
+    /**
+     * Override processImage to modify the colors of the input image
+     * and send it to the output.
+     */
+    public void processImage() {
+        // Get input image information
+        Image inImg = in.getImage();
+        // If there is no input image, clear the output image and finish
+        if (inImg == null) {
+            out.setImage(null);
+            return;
+        }
+        int width   = (int)inImg.getWidth();
+        int height  = (int)inImg.getHeight();
+        PixelReader reader = inImg.getPixelReader();
+
+        // Create a new writable image for the output
+        WritableImage   outImg = new WritableImage(width, height);
+        PixelWriter     writer = outImg.getPixelWriter();
+
+        //Iterate through pixels and adjust color
+        for (int x = 0; x < width; x++) {
+            for (int y = 0; y < height; y++) {
+                Color inColor = reader.getColor(x, y);
+                Color outColor = inColor.deriveColor(
+                    hueSlider.getValue(),                   // hue shift
+                    saturationSlider.getValue() / 100.0,    // saturation factor
+                    brightnessSlider.getValue() / 100.0,    // brightness factor
+                    1.0 // preserve opacity
+                );
+                writer.setColor(x, y, outColor);
+
+            }
+        }
+
+        // Send to output socket
+        out.setImage(outImg);
+    }
+}
---
+++ imgflow/src/main/java/edu/nmsu/imgflow/NodeFactory.java
@@ -0,0 +1,66 @@
+package edu.nmsu.imgflow;
+
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuItem;
+
+/**
+ * Abstract class for the creation of new GraphNodes
+ */
+public abstract class NodeFactory {
+
+    /**
+     * Create and return a new GraphNode of the given type.
+     * Accepted strings for 'type' are:
+     * 'filein','fileout','invert','coloreffects','opacity'
+     * 
+     * If an invalid string is given, null is returned
+     */
+    public static GraphNode createNode(String type) {
+        switch (type) {
+            case "filein":          return new GraphNodeFileIn();
+            case "fileout":         return new GraphNodeFileOut();
+            case "invert":          return new GraphNodeInvert();
+            case "coloreffects":    return new GraphNodeColorEffects();
+            case "opacity":         return new GraphNodeOpacity();
+            case "splitcolor":      return new GraphNodeSplitColor();
+            case "joincolor":       return new GraphNodeJoinColor();
+            default: return null;
+        }
+    }
+
+    /**
+     * Create a JavaFX menu with MenuItems for creating new nodes
+     * and adding them to the active graph.
+     */
+    public static Menu buildNodeCreationMenu() {
+        Menu menu = new Menu("Create");
+
+        menu.getItems().addAll(
+            buildNodeMenuItem("filein",         "File IN"),
+            buildNodeMenuItem("fileout",        "File OUT"),
+            buildNodeMenuItem("invert",         "Invert Colors"),
+            buildNodeMenuItem("coloreffects",   "Color Effects"),
+            buildNodeMenuItem("opacity",        "Opacity"),
+            buildNodeMenuItem("splitcolor",     "Split RGB"),
+            buildNodeMenuItem("joincolor",      "Join RBG")
+        );
+
+        return menu;
+    }
+
+    /**
+     * Create a MenuItem that creates a node of the given type when
+     * selected and adds it to the active graph.
+     */
+    private static MenuItem buildNodeMenuItem(String type, String name) {
+        MenuItem item = new MenuItem(name);
+
+        item.setOnAction((actionEvent) -> {
+            GraphNode node = createNode(type);
+            if (node != null)
+                Main.getInstance().getActiveGraph().getNodes().add(node);
+        });
+
+        return item;
+    }
+}
